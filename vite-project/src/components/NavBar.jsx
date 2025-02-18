3063f1eb6501b24fcfa0411f9073502fc650edd4
harrygould <harry.gould95@gmail.com>
2025-02-18 16:12:44 +1100

    Nav Bar Component

---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../index.css"; // Import your CSS file
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <span className="moving-text">
+        <p>
+          "Compound Interest is the 8th Wonder of the World. He who understands
+          it, earns it ... he who doesn't ... pays it." - Albert Einstein
+        </p>
+      </span>
+    </nav>
+  );
+};
+
+export default NavBar;
