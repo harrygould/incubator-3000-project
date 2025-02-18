d128abf178415b2738a7fa49ef97f9b4ca943312
harrygould <harry.gould95@gmail.com>
2025-02-18 16:14:23 +1100

    App jsx UI commits

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { useState } from "react";
+import CurrentPrice from "./components/CurrentPrice";
+import CurrentHoldings from "./components/CurrentHoldings";
+import NumberOfCoins from "./components/NumberOfCoins";
+import AverageReturns from "./components/AverageReturns";
+import incubator from "./assets/incubator.png";
+import NavBar from "./components/NavBar";
+import FutureGrowth from "./components/FutureGrowth";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [coins, setCoins] = useState(0);
+  const [price, setPrice] = useState(null);
+  const [holdings, setHoldings] = useState(null);
+
+  console.log("App State - Holdings:", holdings);
 
   return (
-    <>
+    <div className="container">
+      <NavBar />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <br></br>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <img className="header-image" src={incubator} />
+      <div className="card2">
+        <h1 className="stage-headers">~ Stage 1 ~</h1>
+        <h2 className="card-descriptions">Your Current Position</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          "You can't really know where you're going until you know where you
+          have been."<br></br>Maya Angelou
         </p>
+        <NumberOfCoins setCoins={setCoins} />
+        <CurrentPrice price={price} setPrice={setPrice} />
+        <CurrentHoldings
+          coins={coins}
+          price={price}
+          setHoldings={setHoldings}
+          holdings={holdings}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="card2">
+        <h1 className="stage-headers">~ Stage 2 ~</h1>
+        <h2 className="card-descriptions">A Historical Context</h2>
+        <p>
+          "Bee’s don’t waste their time explaining to flies that honey tastes
+          better than s**t."<br></br>The Game
+        </p>
+        <AverageReturns />
+      </div>
 
-export default App
+      <div className="card2">
+        <h1 className="stage-headers">~ Stage 3 ~</h1>
+        <h2 className="card-descriptions">The Fun Part</h2>
+        <p>
+          "It is predicted that Bitcoins volatility will decrease over time,
+          returning 29% Compound Annual Growth Rate." <br></br>Michael Saylor
+          (CEO, MicroStrategy)
+        </p>
+        <FutureGrowth currentHoldings={holdings} />
+      </div>
+      <footer>&copy; 2025, Crash Nuts. All rights reserved.</footer>
+    </div>
+  );
+}
